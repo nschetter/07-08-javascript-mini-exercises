f6cc6baf8b28443d2c5cf9ce1a72d943da978625
Sam Stephen <samueljstephen@gmail.com>
2015-07-08 15:15:00 -0500

    now working within tabs2

---
+++ tabs2/global.js
@@ -0,0 +1,12 @@
+// listener
+
+
+
+
+		
+document.getElementById('tabs').addEventListener("click", function(){
+	//get var link = href.value
+	document.getElementById("about").style.display = 'block';
+	document.getElementById("contact").style.display = 'hidden';
+	document.getElementById("portfolio").style.display = 'hidden';
+});
