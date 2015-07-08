f966781b7bc790c5cef59d1e9974a906425589eb
Nick <nschette@yahoo.com>
2015-07-08 15:59:43 -0500

    wip

--- tabs2/global.js
+++ tabs2/global.js
@@ -6,7 +6,16 @@
 		
 document.getElementById('tabs').addEventListener("click", function(){
 	//get var link = href.value
-	document.getElementById("about").style.display = 'block';
-	document.getElementById("contact").style.display = 'hidden';
-	document.getElementById("portfolio").style.display = 'hidden';
+  var link = document.getElementsByTagName('li');
+  console.log(link);
+  var selectedLink = link[0].childNodes[0].getAttribute('href');
+  console.log(selectedLink);
 });
+
+document.getElementById("about").style.display = 'block';
+document.getElementById("contact").style.display = 'none';
+document.getElementById("portfolio").style.display = 'none';
+// function getHash(url) {
+//   var hashPos = url.lastIndexOf ( '#' );
+//   return url.substring( hashPos + 1);
+// }
