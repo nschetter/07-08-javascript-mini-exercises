a5dcfd11818ca7baf0803782cd544d4b5ab70886
Sam Stephen <samueljstephen@gmail.com>
2015-07-08 17:03:27 -0500

    update

--- tabs2/global.js
+++ tabs2/global.js
@@ -1,11 +1,4 @@
-// listener
-
-
-
-
-		
 var tabs = document.getElementsByClassName('tabs');
-
 
 for (var i = 0; i < tabs.length; i++) {
 	tabs[i].addEventListener("click", handle_tab_click);
@@ -15,6 +8,7 @@ function handle_tab_click() {
 	document.getElementById("about").style.display = 'none';
 	document.getElementById("contact").style.display = 'none';
 	document.getElementById("portfolio").style.display = 'none';
+	document.getElementById("resume").style.display = 'none';
 	// this = li object
 	// example: <li class="tabs"><a href="#about">About</a></li>
 	// this.childNodes gives array of anchor tag.
@@ -24,17 +18,3 @@ function handle_tab_click() {
 	
 	document.getElementById(content_id).style.display = 'block';
 }
-//
-//.addEventListener("click", function(){
-//	//get var link = href.value
-//  var link = document.getElementsByTagName('li');
-//  console.log(link);
-//  var selectedLink = link[0].childNodes[0].getAttribute('href');
-//  console.log(selectedLink);
-//});
-//
-//
-// function getHash(url) {
-//   var hashPos = url.lastIndexOf ( '#' );
-//   return url.substring( hashPos + 1);
-// }
