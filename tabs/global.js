9c411ba2a5b97811a71bf4fbc550718c400f47ca
Sam Stephen <samueljstephen@gmail.com>
2015-07-08 13:16:04 -0500

    tomdocd init()

--- tabs/global.js
+++ tabs/global.js
@@ -1,33 +1,59 @@
-var liArray = document.querySelectorAll("li");
 
-
-liArray.addEventListener(MouseEvent.CLICK, showTab);
-
+/*
+ * function init()
+ *
+ * sets up the tabs.
+ */ 
 function init(){
   var tabListItems = document.getElementById('tabs').childNodes;
+  // loops through all the li elements in the tabs unordered list   (.childNodes grabs all li's in ul)
   for (var i = 0; i < tabListItems.length; i++) {
+	// For each li element,
     if ( tabListItems[i].nodeName == "LI") {
+      // it calls the getFirstChildWithTagName() helper function to retrieve the a link element inside.
       var tabLink = getFirstChildWithTagName( tabListItems[i], 'A');
+	  // Then it calls the getHash() helper function to extract the part of the link's URL after the hash;
+	  // this is the ID of the corresponding content div. 
       var id = getHash( tabLink.getAttribute('href'));
+	  // The link element is then stored by ID in the tabLinks array, 
       tabLinks[id] = tabLink;
+	  // and the content div is stored by ID in the contentDivs array.
       contentDivs[id] = document.getElementById(id);
     }
   }
-  var i = 0;
   
+  var i = 0;
+  // It assigns an onclick event handler function called showTab() to each tab link,
   for (var id in tabLinks) {
     tabLinks[id].onclick = showTab;
+	//and highlights the first tab by setting its CSS class to 'selected'.
     tabLinks[id].onfocus = function() {this.blur()};
     if (i == 0) tabLinks[id].className = 'selected';
     i++;
   }
   var i = 0;
-  
+  // It hides all content divs
   for (var id in contentDivs) {
+	// except the first by setting each div's CSS class to 'tabContent hide'.
     if (i != 0) contentDivs[id].className = 'tabContent hide';
     i++;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // function select_tab
