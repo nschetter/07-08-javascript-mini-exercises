fc3ccf7abbd21454ec2eb69c0b8b594c381d69d9
Sam Stephen <samueljstephen@gmail.com>
2015-07-08 16:42:24 -0500

    Tabs2, now understand how to select something clicked and use that.

--- tabs2/global.js
+++ tabs2/global.js
@@ -4,17 +4,36 @@
 
 
 		
-document.getElementById('tabs').addEventListener("click", function(){
-	//get var link = href.value
-  var link = document.getElementsByTagName('li');
-  console.log(link);
-  var selectedLink = link[0].childNodes[0].getAttribute('href');
-  console.log(selectedLink);
-});
+var tabs = document.getElementsByClassName('tabs');
 
-document.getElementById("about").style.display = 'block';
-document.getElementById("contact").style.display = 'none';
-document.getElementById("portfolio").style.display = 'none';
+
+for (var i = 0; i < tabs.length; i++) {
+	tabs[i].addEventListener("click", handle_tab_click);
+}
+
+function handle_tab_click() {
+	document.getElementById("about").style.display = 'none';
+	document.getElementById("contact").style.display = 'none';
+	document.getElementById("portfolio").style.display = 'none';
+	// this = li object
+	// example: <li class="tabs"><a href="#about">About</a></li>
+	// this.childNodes gives array of anchor tag.
+	// this.childNodes[0] <a href="#about">About</a>
+	// "#about"
+	var content_id = this.childNodes[0].getAttribute('href').slice(1);
+	
+	document.getElementById(content_id).style.display = 'block';
+}
+//
+//.addEventListener("click", function(){
+//	//get var link = href.value
+//  var link = document.getElementsByTagName('li');
+//  console.log(link);
+//  var selectedLink = link[0].childNodes[0].getAttribute('href');
+//  console.log(selectedLink);
+//});
+//
+//
 // function getHash(url) {
 //   var hashPos = url.lastIndexOf ( '#' );
 //   return url.substring( hashPos + 1);
