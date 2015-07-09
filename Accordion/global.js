857c5ffbbf4d910c5d3d032ea51b64c9a37e6319
Sam Stephen <samueljstephen@gmail.com>
2015-07-09 09:37:27 -0500

    Accordion works - loops added and traversing on h2 clicked works

--- Accordion/global.js
+++ Accordion/global.js
@@ -1,23 +1,30 @@
 
-//store all h2 attrs (inside div#accordion) in a var and loop thru each, giving them a click
-
-
+// store all h2 attrs (inside div#accordion) in a var
 var tabs = document.getElementsByTagName('h2');
+
+// array of all the divs that are inside accordion
 var contentDivs = document.getElementById("accordion").getElementsByTagName('div');
 
-
+// loop thru each, giving them a click
 for (var i = 0; i < tabs.length; i++) {
 	tabs[i].addEventListener("click", handle_tab_click);
 }	
 
-function handle_tab_click() {
-	contentDivs[0].style.display = 'none';
-	contentDivs[1].style.display = 'none';
-	contentDivs[2].style.display = 'none';
-	contentDivs[3].style.display = 'none';
-	var content_id = this.childNodes[0].getAttribute('href').slice(1);
-	
-	document.getElementById(content_id).getElementByTagName('div')[0].style.display = 'block';
+// handle_tab_click loops thru each div and hides them.
+// this loop will make sure that the first content will always show when page loads
+for (var n = 1; n < contentDivs.length; n++) {
+	contentDivs[n].style.display = 'none';
 }
 
+function handle_tab_click() {
+	// handle_tab_click loops thru each div and hides them... all of them this time
+	for (var n = 0; n < contentDivs.length; n++) {
+		contentDivs[n].style.display = 'none';
+	}
+	
+	// now we store the element (a div) after the h2 the user has clicked.
+	var div_after_h2 = this.nextElementSibling;
+	// using the div we have stored, we change the display to block!
+	div_after_h2.style.display = 'block';
+}
 
